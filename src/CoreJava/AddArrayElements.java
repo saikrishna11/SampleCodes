1fb281cfd8d78452129afff0db543a6228c48d78
saikrishna <saikrishna92a@gmail.com>
2017-03-05 02:47:48 -0500

    Modified

--- src/CoreJava/AddArrayElements.java
+++ src/CoreJava/AddArrayElements.java
@@ -2,21 +2,22 @@ package CoreJava;
 
 import java.util.Scanner;
 
-//This class is able to print the sum of the array elements in the array
+//Write a program to print the sum of all the array elements in the array
 
 public class AddArrayElements {
 
 	public static void main(String[] args) {
-		System.out.println("Enter the input array: ");
+		System.out.println("Enter the size of input array: ");
 		Scanner sc = new Scanner(System.in);
-		int[] array = new int[5];
+		int size = sc.nextInt();
+		System.out.println("Enter the elements of input array: ");
+		int[] array = new int[size];
 		int sum = 0;
 		for(int i = 0; i<array.length; i++)
 		{
 			array[i] = sc.nextInt();
 			sum+=array[i];
 		}
-		System.out.println("Total: "+sum);
+		System.out.println("The sum of all the elements in this array is : "+sum);
 	}
-
 }
